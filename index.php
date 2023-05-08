1e9bf883ba3dfe0d6e70c012cd136ec244a8d02b
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-08 17:27:58 +0200

    Add some style

--- index.php
+++ index.php
@@ -8,22 +8,34 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <link href='./style.css' rel='stylesheet'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
 <body>
   <div id='app'>
-    <h1>TO-DO LIST</h1>
+    <div class="container text-center">
+      <h2>TO-DO LIST</h2>
+      <div class="row">
+        <div class="col">
+          <div class="card mx-auto my-3" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+              <li v-for="(task, index) in tasks"
+                class="list-group-item d-flex justify-content-between align-items-center">
+                <span>{{ task }}</span>
+                <button @click="deleteTask(index)" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+              </li>
+            </ul>
+          </div>
+          <div class="input-group mb-3 mx-auto">
+            <input type="text" class="form-control" v-model="newTask" @keyup.enter="addTask"
+              placeholder="Add a task">
+            <span class="input-group-text" id="basic-addon1" @click="addTask">Add Task</span>
+          </div>
 
-    <ul>
-      <li v-for="(task, index) in tasks">
-        {{ task }}
-        <button @click="deleteTask(index)">Delete</button>
-      </li>
-    </ul>
+        </div>
+      </div>
 
 
-    <div class="addTask">
-      <input type="text" v-model="newTask" @keyup.enter="addTask" placeholder="Add a task">
     </div>
 
 
