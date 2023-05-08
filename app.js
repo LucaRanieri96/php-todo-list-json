c2884e08c11f335b46aa1ce8dd37b6c6e7b8c3e3
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-08 15:30:06 +0200

    Add addTask method in app.js to post task to api

--- app.js
+++ app.js
@@ -1,26 +1,40 @@
-const { createApp } = Vue
+const { createApp } = Vue;
 createApp({
   data() {
     return {
       tasks: null,
-      api_url: 'getTasks.php',
-      store_url: 'storeTasks.php',
-    }
+      api_url: "getTasks.php",
+      store_url: "storeTasks.php",
+      newTask: "",
+    };
   },
+  //   mi serve un metodo per pushare le nuove task dentro l'array nell'api, attraverso storeTasks
   methods: {
+    addTask() {
+      // uso sempre axios come ho fatto sotto, invece di get per ottenere i dati dall'api userò post per inserirceli e seguirò la procedura inversa di storeTasks.php
+      axios
+        .post(this.store_url, this.newTask, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((response) => {
+          console.log(response);
+          this.newTask = response.data;
+        })
+        .catch((error) => {
+          console.error(error.message);
+        });
+    },
   },
   mounted() {
-
+    // uso axios per prendere il contenuto dall'api "getTasks" e metterle in "tasks"
     axios
       .get(this.api_url)
-      .then(response => {
+      .then((response) => {
         console.log(response);
-        this.tasks = response.data
+        this.tasks = response.data;
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error.message);
-      })
-
-
-  }
-}).mount('#app')
+      });
+  },
+}).mount("#app");
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1'>
   <title>Todo List</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   <link href='./style.css' rel='stylesheet'>
 </head>
 
@@ -19,9 +20,9 @@
       </li>
     </ul>
 
-    
+
     <div class="add_task">
-      <input type="text" v-model="new_task" @keyup.enter="add_task" placeholder="Type a task here">
+      <input type="text" v-model="new_task" @keyup.enter="addTask" placeholder="Add a task">
     </div>
 
 
@@ -30,7 +31,9 @@
   <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
   <!-- Development only cdn, disable in production -->
   <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+    crossorigin="anonymous"></script>
   <script src='./app.js'></script>
 </body>
 
