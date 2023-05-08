4c40caad7b871aff6b57f9df94db219d5cc14cbf
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-08 14:40:06 +0200

    Init project

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang='en'>
+
+<head>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <title>Tasks List</title>
+  <link href='./style.css' rel='stylesheet'>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+  <div id='app'>
+    <h1>TO-DO LIST</h1>
+
+    <ul>
+      <li>
+      </li>
+    </ul>
+
+    
+    <div class="add_task">
+      <input type="text" v-model="new_task" @keyup.enter="add_task" placeholder="Type a task here">
+    </div>
+
+
+  </div>
+
+  <script src='https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js'></script>
+  <!-- Development only cdn, disable in production -->
+  <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
