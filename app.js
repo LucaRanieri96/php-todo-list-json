f8a2cece3b29933f09639d6bff08a5203d92a2a0
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-08 15:01:46 +0200

    Add app.js to get tasks from api

---
+++ app.js
@@ -0,0 +1,25 @@
+const { createApp } = Vue
+createApp({
+  data() {
+    return {
+      tasks: null,
+      api_url: 'getTasks.php',
+    }
+  },
+  methods: {
+  },
+  mounted() {
+
+    axios
+      .get(this.api_url)
+      .then(response => {
+        console.log(response);
+        this.tasks = response.data
+      })
+      .catch(error => {
+        console.error(error.message);
+      })
+
+
+  }
+}).mount('#app')
