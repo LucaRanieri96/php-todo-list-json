9a81cbd6c772d71ce9678c4a74799205ba27ca3b
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-09 14:35:53 +0200

    Revert "Fix style"
    This reverts commit 28091b469f983a45a041b1e8e232d74c47f85042.

--- index.php
+++ index.php
@@ -8,12 +8,8 @@
   <title>Todo List</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <link href='./style.css' rel='stylesheet'>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600&display=swap"
-    rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 
 <body>
@@ -22,7 +18,7 @@
       <h2>TO-DO LIST</h2>
       <div class="row">
         <div class="col">
-          <div class="card my-5">
+          <div class="card mx-auto my-3" style="width: 18rem;">
             <ul class="list-group list-group-flush">
               <li v-for="(task, index) in tasks" :class="task.done === 'true' ? 'done' : '' "
                 class="list-group-item d-flex justify-content-between align-items-center">
